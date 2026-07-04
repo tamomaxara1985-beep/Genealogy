5844989b65426b95bd74d55ab75744119f3a8480
tamomaxara1985-beep <tamo.maxara1985@gmail.com>
2026-07-04 17:31:48 +0400

    feat: global SWR polling + focus/reconnect revalidation for live cross-tab updates

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,7 +1,21 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { SWRConfig } from "swr";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <SWRConfig
+        value={{
+          refreshInterval: 20000,
+          revalidateOnFocus: true,
+          revalidateOnReconnect: true,
+          dedupingInterval: 5000,
+        }}
+      >
+        {children}
+      </SWRConfig>
+    </SessionProvider>
+  );
 }
